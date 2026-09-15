8bc7f0311e1d0cebe464a1dd77c8862c94ef4a4e
Rozaaq-prog <rizalsuki113@gmail.com>
2026-09-15 22:31:22 +0800

    Add files via upload

---
+++ produk Laptop.py
@@ -0,0 +1,14 @@
+produk = {
+    "nama": "Laptop asus",
+    "harga": 7500000,
+    "stok": 10
+}
+
+print("produk = ", produk)
+
+print("1. Tampilkan Data Produk")
+print("2. Tambah Kategori")
+print("3. Ubah Harga Produk")
+print("4. Hapus Kategori")
+print("5. Tampilkan Data Setelah Perubahan")
+print("6. Keluar")
